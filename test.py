20246c050c371e78b105f7ae64ab904f283e6113
Evgeni1494 <yoni1@hotmail.fr>
2023-07-05 15:21:18 +0200

    ElasticSearch Requetages

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import nltk
-import time
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pickle
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
@@ -13,51 +13,39 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
-from elasticsearch import Elasticsearch
-from faker import Faker
 
 
-
-# Connexion à Elasticsearch
-es = Elasticsearch(hosts=["http://localhost:9200"])
-
-# Mapping de l'index "notes"
-mapping = {
-    "mappings": {
-        "properties": {
-            "patient_lastname": {"type": "keyword"},
-            "patient_firstname": {"type": "keyword"},
-            "text": {"type": "text", "analyzer": "standard"},
-            "date": {"type": "date"},
-            "patient_left": {"type": "boolean"},
-            "emotion": {"type": "keyword"},
-            "confidence": {"type": "float"}
-        }
-    }
-}
-
-# Création de l'index avec le mapping
-index_name = "notes5"
-es.indices.create(index=index_name, body=mapping)
+data=pd.read_csv('Emotion_final.csv')
 
 
-df = pd.read_csv("Predicted_Data.csv")
-fake = Faker()
+# Étape 3 : Prétraitement des données
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
 
-# Alimentation de l'index "notes"
-for index, row in df.iterrows():
-    doc = {
-        "patient_lastname": fake.last_name(),
-        "patient_firstname": fake.first_name(),
-        "text": row["Text"],
-        "emotion": row["Emotion"],
-        "date": fake.date(),
-        "patient_left": fake.boolean(),
-        "confidence": row['Confidence']
-    }
-    es.index(index=index_name, body=doc)
-es.transport.close()
+def preprocess_data(text):
+    # Tokenisation
+    tokens = nltk.word_tokenize(text.lower())
+    
+    # Suppression de la ponctuation
+    tokens = [token for token in tokens if token.isalnum()]
+    
+    # Suppression des stopwords
+    tokens = [token for token in tokens if token not in stop_words]
+    
+    # Lemmatisation
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    
+    # Reconstitution du texte prétraité
+    preprocessed_text = ' '.join(tokens)
+    
+    return preprocessed_text
 
+data['preprocessed_text'] = data['Text'].apply(preprocess_data)
+
+# Étape 4 : Création du Bag of Words
+vectorizer = CountVectorizer()
+bag_of_words = vectorizer.fit_transform(data['preprocessed_text'])
+feature_names = vectorizer.get_feature_names_out()
 
 
 
